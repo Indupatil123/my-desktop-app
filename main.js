2e4605255445647419f0b2578c46abe10e5c8d7c
Indu Reddy <indureddy@gmail.com>
2026-02-17 21:44:16 +0530

    Initial clean commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow, shell } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  // Load existing website
+  win.loadURL("https://www.youtube.com");
+
+  // Open external links in browser
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: "deny" };
+  });
+}
+
+app.whenReady().then(createWindow);
